3acc8e14b37fd0dd6dfce64a3b35932736a12c94
Ryan Bobo <ryanbobo@txstate.edu>
2020-11-03 16:53:02 -0600

    additional weather app css

--- JavaScript/external.js
+++ JavaScript/external.js
@@ -58,11 +58,19 @@ window.onload = function(){
 }
 
 function drawWeather( d ) {
-  var celcius = Math.round(parseFloat(d.main.temp)-273.15);
-  var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32);
+	var celcius = Math.round(parseFloat(d.main.temp)-273.15);
+	var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32);
+	var description = d.weather[0].description;
 
-  document.getElementById('description').innerHTML = d.weather[0].description;
-  document.getElementById('temp').innerHTML = fahrenheit + '&deg;';
-  document.getElementById('location').innerHTML = d.name;
-  //document.getElementByID('precip').innerHTML = d.
+	document.getElementById('description').innerHTML = description;
+	document.getElementById('temp').innerHTML = fahrenheit + '&deg;';
+	document.getElementById('location').innerHTML = d.name;
+
+	if( description.indexOf('rain') > 0 ) {
+  	document.body.className = 'rainy';
+  } else if( description.indexOf('cloud') > 0 ) {
+  	document.body.className = 'cloudy';
+  } else if( description.indexOf('sunny') > 0 ) {
+  	document.body.className = 'sunny';
+  }
 }

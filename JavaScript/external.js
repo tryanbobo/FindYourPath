5b1ca975ce5ff31352a02d813117bc92d56690d3
Ryan Bobo <ryanbobo@txstate.edu>
2020-11-17 10:39:17 -0600

    Trails query

--- JavaScript/external.js
+++ JavaScript/external.js
@@ -113,7 +113,84 @@ require([
       popupTemplate: popupTrails
   });
   map.add(trailsLayer);
+/*
+  const listNode = document.getElementById("x");
 
+  let graphics;
+
+  view.whenLayerView(trailsLayer).then(function (layerView) {
+          layerView.watch("updating", function (value) {
+            if (!value) {
+              // wait for the layer view to finish updating
+
+              // query all the features available for drawing.
+              layerView
+                .queryFeatures({
+                  geometry: view.extent,
+                  returnGeometry: true,
+                  orderByFields: ["FID"]
+                })
+                .then(function (results) {
+                  graphics = results.features;
+
+                  const fragment = document.createDocumentFragment();
+
+                  graphics.forEach(function (result, index) {
+                    const attributes = result.attributes;
+                    const name = attributes.NAME;
+
+                    // Create a list zip codes in NY
+                    const li = document.createElement("li");
+                    li.classList.add("panel-result");
+                    li.tabIndex = 0;
+                    li.setAttribute("data-result-id", index);
+                    li.textContent = name;
+
+                    fragment.appendChild(li);
+                  });
+                  // Empty the current list
+                  listNode.innerHTML = "";
+                  listNode.appendChild(fragment);
+                })
+                .catch(function (error) {
+                  console.error("query failed: ", error);
+                });
+            }
+          });
+        });
+
+        // listen to click event on the zip code list
+        listNode.addEventListener("click", onListClickHandler);
+
+        function onListClickHandler(event) {
+          const target = event.target;
+          const resultId = target.getAttribute("data-result-id");
+
+          // get the graphic corresponding to the clicked zip code
+          const result =
+            resultId && graphics && graphics[parseInt(resultId, 10)];
+
+          if (result) {
+            // open the popup at the centroid of zip code polygon
+            // and set the popup's features which will populate popup content and title.
+
+            view
+              .goTo(result.geometry.extent.expand(2))
+              .then(function () {
+                view.popup.open({
+                  features: [result],
+                  location: result.geometry.centroid
+                });
+              })
+              .catch(function (error) {
+                if (error.name != "AbortError") {
+                  console.error(error);
+                }
+              });
+          }
+        }
+
+/*******************************************************************/
 
   var track = new Track({
     view: view  //assigns tracker to current map view
